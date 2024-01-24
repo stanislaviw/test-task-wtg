1b2d5904affe6c2f499838659e5cad0b859a2e6c
stanislaviw <slavi8ok@gmail.com>
2024-01-24 11:49:58 +0200

    reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import "./App.css";
+import { Header } from "./layout/header";
 
 export const App = (): JSX.Element => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <Header />
+      <div className="content"></div>
+    </div>
+  );
 };
---
+++ src/layout/header.tsx
@@ -0,0 +1,3 @@
+export const Header = (): JSX.Element => {
+  return <div className="header">lol</div>;
+};
