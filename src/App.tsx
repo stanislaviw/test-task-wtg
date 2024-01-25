27efb05e6ed4d059fc2892a967055d9937465b86
stanislaviw <slavi8ok@gmail.com>
2024-01-25 14:01:39 +0200

    finished 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import "./App.css";
 import { Header } from "./layout/header";
 import { List } from "./list/list";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 export const App = (): JSX.Element => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <div className="container">
-        <Header />
-        <main className="content">
-          <List />
-        </main>
-      </div>
-    </LocalizationProvider>
+    <div className="container">
+      <Header />
+      <main className="content">
+        <List />
+      </main>
+    </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <App />
+    </LocalizationProvider>
   </React.StrictMode>
 );
